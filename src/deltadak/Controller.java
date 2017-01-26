a51c78e9e2131e3903c58ec3b963a24174415fc6
Thomas <hollandpirates@gmail.com>
2017-01-26 21:00:07 +0100

    trying to make items editable

--- src/deltadak/Controller.java
+++ src/deltadak/Controller.java
@@ -5,11 +5,14 @@ import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.cell.TextFieldListCell;
 import javafx.scene.input.*;
+import javafx.scene.layout.GridPane;
 import javafx.util.Callback;
+import javafx.util.StringConverter;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -27,40 +30,41 @@ public class Controller implements Initializable {
      */
     @FXML ListView<String> day1;
     @FXML ListView<String> day2;
-    ObservableList<String> day1List = FXCollections.<String>observableArrayList("task1","task2");
-    ObservableList<String> day2List = FXCollections.<String>observableArrayList("task3","task4");
-
+    @FXML GridPane gridpane;
 
     /**
      * Initialization method for the controller.
      */
     @FXML public void initialize(URL location, ResourceBundle resourceBundle){
 
-
+        //some debug defaults
+        ObservableList<String> day1List = FXCollections.<String>observableArrayList("task1","task2");
+        ObservableList<String> day2List = FXCollections.<String>observableArrayList("task3","task4");
         day1.setItems(day1List);
-        day1.setCellFactory(TextFieldListCell.forListView());
-        day1.setOnEditCommit(new EventHandler<ListView.EditEvent<String>>(){
-            @Override
-            public void handle(ListView.EditEvent<String> t) {
-                day1.getItems().set(t.getIndex(), t.getNewValue());
-            }
-        });
-
         day2.setItems(day2List);
-        day2.setCellFactory(TextFieldListCell.forListView());
-        day2.setOnEditCommit(new EventHandler<ListView.EditEvent<String>>(){
-            @Override
-            public void handle(ListView.EditEvent<String> t) {
-                day2.getItems().set(t.getIndex(), t.getNewValue());
-            }
-        });
 
-        //todo setup drag and drop for all children of gridview
-        setupDragAndDrop(day1);
-        setupDragAndDrop(day2);
+        //setup drag and drop for all children of gridview
+        for (Node node : gridpane.getChildren()) {
+            if (node instanceof ListView) {
+                ListView<String> list = (ListView<String>) node;
+                setupDragAndDrop(list);
+                //todo enables editing but overrides drag and drop
+                //todo merge the callback and this callback below?
+                list.setCellFactory(TextFieldListCell.forListView());
+                list.setOnEditCommit(new EventHandler<ListView.EditEvent<String>>(){
+                    @Override
+                    public void handle(ListView.EditEvent<String> t) {
+                        list.getItems().set(t.getIndex(), t.getNewValue());
+                    }
+                });
+            }
+        }
     }
 
     private void setupDragAndDrop(ListView<String> list) {
+        //todo cellfactory needs to produce editable listcell?
+        //like new TextFieldListCell<String>(new DefaultListConverter())
+        //see implementation of forListView
         list.setCellFactory(new Callback<ListView<String>, ListCell<String>>() {
             @Override
             public ListCell<String> call(ListView<String> param) {
@@ -111,11 +115,9 @@ public class Controller implements Initializable {
                 listCell.setOnDragDropped(new EventHandler<DragEvent>() {
                     @Override
                     public void handle(DragEvent event) {
-                        System.out.println(listCell.getItem());
                         Dragboard db = event.getDragboard();
                         boolean success = false;
                         if (db.hasString()) {
-                            System.out.println("Dropped on "+listCell.getIndex());
                             String newvalue = db.getString();
                             //insert new task, removing will happen in onDragDone
                             int index = min(listCell.getIndex(),list.getItems().size()); // item can be dropped way below the existing list
@@ -141,7 +143,6 @@ public class Controller implements Initializable {
                             Dragboard db = event.getDragboard();
                             String draggedvalue = db.getString();
                             //remove original item
-                            System.out.println("removing item originally at "+listCell.getIndex());
                             //item can have been moved up (so index becomes one too much)
                             // or such that the index didn't change, like to another day
                             if (list.getItems().get(listCell.getIndex()).equals(draggedvalue)) {
@@ -149,7 +150,7 @@ public class Controller implements Initializable {
                             } else {
                                 list.getItems().remove(listCell.getIndex()+1);
                             }
-                            //prevent an empty list from refusing to receive items //// TODO
+                            //prevent an empty list from refusing to receive items, as it wouldn't contain any listcell
                             if (list.getItems().size() < 1) {
                                 list.getItems().add("");
                             }
