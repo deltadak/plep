00a8e182807f62b68ef2c7885ef3705819af418e
Thomas <hollandpirates@gmail.com>
2017-01-26 15:40:59 +0100

    added basic drag and drop

--- src/deltadak/Controller.java
+++ src/deltadak/Controller.java
@@ -1,6 +1,20 @@
 package deltadak;
 
-public class Controller {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.cell.TextFieldListCell;
+import javafx.scene.input.*;
+import javafx.util.Callback;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
     /**
      * database communication below
      */
@@ -9,5 +23,130 @@ public class Controller {
     /**
      * interface functions below
      */
+    @FXML ListView<String> day1;
+    @FXML ListView<String> day2;
+    ObservableList<String> day1List = FXCollections.<String>observableArrayList("task1","task2");
+    ObservableList<String> day2List = FXCollections.<String>observableArrayList("task3","task4");
+
+
+    /**
+     * Initialization method for the controller.
+     */
+    @FXML public void initialize(URL location, ResourceBundle resourceBundle){
+
+
+        day1.setItems(day1List);
+        day1.setCellFactory(TextFieldListCell.forListView());
+        day1.setOnEditCommit(new EventHandler<ListView.EditEvent<String>>(){
+            @Override
+            public void handle(ListView.EditEvent<String> t) {
+                day1.getItems().set(t.getIndex(), t.getNewValue());
+            }
+        });
+
+        day2.setItems(day2List);
+        day2.setCellFactory(TextFieldListCell.forListView());
+        day2.setOnEditCommit(new EventHandler<ListView.EditEvent<String>>(){
+            @Override
+            public void handle(ListView.EditEvent<String> t) {
+                day2.getItems().set(t.getIndex(), t.getNewValue());
+            }
+        });
+
+        setupDragAndDrop(day1);
+        setupDragAndDrop(day2);
+    }
+
+    private void setupDragAndDrop(ListView<String> list) {
+        list.setCellFactory(new Callback<ListView<String>, ListCell<String>>() {
+            @Override
+            public ListCell<String> call(ListView<String> param) {
+                ListCell<String> listCell = new ListCell<String>() {
+                    @Override
+                    protected void updateItem( String item, boolean empty) {
+                        super.updateItem(item, empty);
+                        setText(item);
+                    }
+                };
+
+                listCell.setOnDragDetected( (MouseEvent event) -> {
+                    Dragboard db = listCell.startDragAndDrop(TransferMode.COPY);
+                    ClipboardContent content = new ClipboardContent();
+                    content.putString( listCell.getItem());
+                    db.setContent(content);
+                    event.consume();
+                });
+
+                listCell.setOnDragOver(new EventHandler<DragEvent>() {
+                    @Override
+                    public void handle(DragEvent event) {
+                        if (event.getGestureSource() != listCell && event.getDragboard().hasString()) {
+                            event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+                        }
+                        event.consume();
+                    }
+                });
+
+                listCell.setOnDragEntered(new EventHandler<DragEvent>() {
+                    @Override
+                    public void handle(DragEvent event) {
+                        if (event.getGestureSource() != listCell && event.getDragboard().hasString()) {
+                            System.out.println("TODO: change color of listview"); //todo
+                        }
+                        event.consume();
+                    }
+                });
+
+                listCell.setOnDragExited(new EventHandler<DragEvent>() {
+                    @Override
+                    public void handle(DragEvent event) {
+                        System.out.println("TODO reset color of listview"); //todo
+                        event.consume();
+                    }
+                });
+
+                listCell.setOnDragDropped(new EventHandler<DragEvent>() {
+                    @Override
+                    public void handle(DragEvent event) {
+                        System.out.println(listCell.getItem());
+                        Dragboard db = event.getDragboard();
+                        boolean success = false;
+                        if (db.hasString()) {
+                            System.out.println("Dropped on "+listCell.getIndex());
+                            String newvalue = db.getString();
+                            //insert new task, removing will happen in onDragDone
+                            list.getItems().add(listCell.getIndex(),newvalue);
+                            success = true;
+                        }
+                        event.setDropCompleted(success);
+                        event.consume();
+                    }
+                });
+
+                listCell.setOnDragDone(new EventHandler<DragEvent>() {
+                    @Override
+                    public void handle(DragEvent event) {
+//                        if (event.getTransferMode() == TransferMode.COPY_OR_MOVE) {
+                            Dragboard db = event.getDragboard();
+                            String draggedvalue = db.getString();
+                            //remove original item
+                            System.out.println("removing item originally at "+listCell.getIndex());
+                            //item can have been moved up (so index becomes one too much)
+                            // or such that the index didn't change, like to another day
+                            if (list.getItems().get(listCell.getIndex()).equals(draggedvalue)) {
+                                list.getItems().remove(listCell.getIndex());
+                            } else {
+                                list.getItems().remove(listCell.getIndex()+1);
+                            }
+//                        }
+                        event.consume();
+                    }
+                });
+
+                return listCell;
+            }
+        });
+    }
+
 
 }
--- src/deltadak/Main.java
+++ src/deltadak/Main.java
@@ -2,8 +2,10 @@ package deltadak;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,8 +13,16 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("interface.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Plep");
         primaryStage.setScene(new Scene(root, 300, 275));
+
+        //set a size relative to screen
+        Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
+        //set Stage boundaries to visible bounds of the main screen
+        primaryStage.setX(primaryScreenBounds.getMinX());
+        primaryStage.setY(primaryScreenBounds.getMinY());
+        primaryStage.setWidth(primaryScreenBounds.getWidth()/2);
+        primaryStage.setHeight(primaryScreenBounds.getHeight());
         primaryStage.show();
     }
 
