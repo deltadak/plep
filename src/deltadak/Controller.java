9fd33572b45e5ac0f56244ccedbe674f8fdd08d2
Thomas <hollandpirates@gmail.com>
2017-02-05 15:37:20 +0100

    pushing work so far

--- src/deltadak/Controller.java
+++ src/deltadak/Controller.java
@@ -12,20 +12,20 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Priority;
 import javafx.util.Callback;
+import javafx.util.StringConverter;
+import javafx.util.converter.DefaultStringConverter;
 
 import java.io.Serializable;
 import java.net.URL;
 import java.util.ResourceBundle;
-import java.util.function.BiFunction;
-import java.util.function.Function;
 
 import static java.lang.Integer.min;
 
 public class Controller implements Initializable {
     @FXML ListView<Task> day1;
-    @FXML ListView<String> day2;
+    @FXML ListView<Task> day2;
     @FXML GridPane gridpane;
-    DataFormat dataFormat = new DataFormat("com.deltadak.Task");
+    private DataFormat dataFormat = new DataFormat("com.deltadak.Task");
 
     /**
      * Initialization method for the controller.
@@ -47,12 +47,15 @@ public class Controller implements Initializable {
 
         //some debug defaults
         ObservableList<Task> day1Tasks = FXCollections.observableArrayList(
-                new Task("task1","2WA70"),
-                new Task("task2","0LAUK0")
+                new Task("gdv","2WA70"),
+                new Task("methods","2IPC0")
         );
-//        ObservableList<String> day2List = FXCollections.observableArrayList("task3","task4","","","","");
+        ObservableList<Task> day2Tasks = FXCollections.observableArrayList(
+                new Task("discrete","2WF50"),
+                new Task("robot","0LAUK0")
+        );
         day1.setItems(day1Tasks);
-//        day2.setItems(day2List);
+        day2.setItems(day2Tasks);
 
         //setup drag and drop for all children of gridview
         gridpane.getChildren().stream().filter(node -> node instanceof ListView).forEach(node -> {
@@ -69,6 +72,8 @@ public class Controller implements Initializable {
             @Override
             public LabelCell call(ListView<Task> param) {
                 LabelCell labelCell = new LabelCell() {};
+
+                labelCell.setConverter(new TaskConverter(labelCell));
 
                 labelCell.setOnDragDetected( (MouseEvent event) -> {
                     if (!labelCell.getItem().getText().equals("")) {
@@ -130,8 +135,10 @@ public class Controller implements Initializable {
                         // or such that the index didn't change, like to another day
                         if (list.getItems().get(labelCell.getIndex()).getText().equals(newTask.getText())) {
                             list.getItems().set(labelCell.getIndex(),emptyTask);
+                            labelCell.setGraphic(null);
                         } else {
                             list.getItems().set(labelCell.getIndex()+1,emptyTask);
+//                            nextLabelCell.setGraphic(null);
                         }
                         //prevent an empty list from refusing to receive items, as it wouldn't contain any listcell
                         if (list.getItems().size() < 1) {
@@ -146,69 +153,63 @@ public class Controller implements Initializable {
         });
     }
 
-    static class LabelCell extends TextFieldListCell<Task> {
+    private class LabelCell extends TextFieldListCell<Task> {
         HBox hbox = new HBox();
-        TextField textField = new TextField("(empty)");
+        Label text = new Label("");
         Pane pane = new Pane();
         ObservableList<String> comboList = FXCollections.observableArrayList(
                 "0LAUK0","2WF50","2WA70","2IPC0");
         ComboBox<String> comboBox = new ComboBox<>(comboList);
 
-        public LabelCell() {
+        private LabelCell() {
             super();
-            hbox.getChildren().addAll(textField, pane, comboBox);
+            hbox.getChildren().addAll(text, pane, comboBox);
             HBox.setHgrow(pane, Priority.ALWAYS);
-
-            textField.setOnAction(e -> {
-                Task newItem = getItem();
-                newItem.setText(textField.getText());
-                newItem.setLabel(comboBox.getValue());
-                commitEdit(newItem);
-            });
-
         }
 
+        /**
+         * called when starting edit with (null, true)
+         * and when finished edit with (task, false)
+         * @param task
+         * @param empty
+         */
         @Override
         public void updateItem(Task task, boolean empty) {
             super.updateItem(task, empty);
-            if (empty || task == null) {
+            setText(null);
+            if (empty) {
                 setGraphic(null);
-                setText(null); // No text in textField of super class
-            } else if (isEditing()) {
-                textField.setText(task.getText());
-                setText(null);
-                setGraphic(hbox);
             } else {
-                setText(task.getText());
-                setGraphic(null);
+                text.setText(task.getText() != null ? task.getText() : "<null>");
+                comboBox.setValue(task.getLabel() != null ? task.getLabel() : "<null>");
+                setGraphic(hbox);
             }
         }
+    }
 
+    /**
+     * custom stringconverter to define what editing a listcell means
+     * this converter is set on each listcell
+     */
+    private class TaskConverter extends StringConverter<Task> {
+        private final ListCell<Task> cell;
+        private TaskConverter(ListCell<Task> cell) {
+            this.cell = cell;
+        }
         @Override
-        public void startEdit() {
-            super.startEdit();
-            textField.setText(getItem().getText());
-            setText(null);
-            setGraphic(hbox);
-            textField.selectAll();
-            textField.requestFocus();
+        public String toString(Task task) {
+            return task.getText();
         }
 
         @Override
-        public void cancelEdit() {
-            super.cancelEdit();
-            setText(getItem().getText());
-            setGraphic(null);
-        }
-
-        @Override
-        public void commitEdit(Task task) {
-            super.commitEdit(task);
-            getListView().getItems().set(getIndex(), task);
-            setText(getItem().getText());
-            setGraphic(null);
+        public Task fromString(String string) {
+            Task task = cell.getItem();
+            task.setText(string);
+            return task;
         }
     }
+
+
     //when transferred with the dragboard, the object is serialized
     //which I think means that a new object is created and you lose the reference to the old one
     //which I think should be fine here, as only content matters
@@ -216,24 +217,24 @@ public class Controller implements Initializable {
         private String text;
         private String label;
 
-        public Task(String text, String label) {
+        private Task(String text, String label) {
             this.text = text;
             this.label = label;
         }
 
-        public String getText() {
+        private String getText() {
             return text;
         }
 
-        public void setText(String text) {
+        private void setText(String text) {
             this.text = text;
         }
 
-        public String getLabel() {
+        private String getLabel() {
             return label;
         }
 
-        public void setLabel(String label) {
+        private void setLabel(String label) {
             this.label = label;
         }
     }
