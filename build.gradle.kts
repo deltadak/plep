a453b06cacd075c184c26997d536f545411d7ba0
Thomas <hollandpirates@gmail.com>
2020-01-25 17:00:52 +0100

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,19 +14,19 @@ plugins {
     id("edu.sc.seis.launch4j") version "2.4.6"
 
     // Plugin to build fat jars
-    id("com.github.johnrengelman.shadow") version "5.1.0"
+    id("com.github.johnrengelman.shadow") version "5.2.0"
 
     // help/dependencyUpdates checks for dependency updates.
-    id("com.github.ben-manes.versions") version "0.22.0"
+    id("com.github.ben-manes.versions") version "0.27.0"
 
     // help/useLatestVersions updates dependency versions
-    id("se.patrikerdes.use-latest-versions") version "0.2.12"
+    id("se.patrikerdes.use-latest-versions") version "0.2.13"
 
     // Code coverage
     jacoco
 
     // Upload jacoco coverage reports to coveralls
-    id("com.github.kt3k.coveralls") version "2.8.4"
+    id("com.github.kt3k.coveralls") version "2.9.0"
 }
 
 application {
@@ -39,52 +39,52 @@ dependencies {
     // https://mvnrepository.com/artifact/org.xerial/sqlite-jdbc
     // 'compile' is deprecated, now it is 'api' but that fails to build with
 //    Could not find method api() for arguments [org.xerial:sqlite-jdbc:3.18.0] on object of type org.gradle.api.internal.artifacts.dsl.dependencies.DefaultDependencyHandler.
-    compile("org.xerial:sqlite-jdbc:3.28.0")
+    implementation("org.xerial:sqlite-jdbc:3.30.1")
 
     // Database driver (for possible future use with Exposed).
-    compile("com.h2database:h2:1.4.199")
+    implementation("com.h2database:h2:1.4.200")
 
     // JNA, used to e.g. make a program pinnable to task bar.
-    compile("net.java.dev.jna:jna:5.4.0")
-    compile("net.java.dev.jna:jna-platform:5.4.0")
+    implementation("net.java.dev.jna:jna:5.5.0")
+    implementation("net.java.dev.jna:jna-platform:5.5.0")
 
     // Kotlin
-    compile(kotlin("stdlib:1.3.11"))
+    implementation(kotlin("stdlib:1.3.11"))
 
     // To "prevent strange errors".
-    compile(kotlin("reflect:1.3.11"))
+    implementation(kotlin("reflect:1.3.11"))
 
     // Kotlin reflection.
-    compile(kotlin("test"))
-    compile(kotlin("test-junit"))
+    implementation(kotlin("test"))
+    implementation(kotlin("test-junit"))
 
     // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0-RC2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-javafx:1.3.0-RC2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-javafx:1.3.3")
 
     // Kotlin Exposed SQL DSL
-    compile("org.jetbrains.exposed:exposed:0.16.3")
-    compile("org.slf4j:slf4j-simple:2.0.0-alpha0")
+    implementation("org.jetbrains.exposed:exposed:0.17.7")
+    implementation("org.slf4j:slf4j-simple:2.0.0-alpha1")
 
     // JUnit 5
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.5.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.5.1")
-    testRuntime("org.junit.platform:junit-platform-console:1.5.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-console:1.6.0")
 
     // Kotlintests are not run anyway when using JUnit 5 as well.
-    testCompile("io.kotlintest:kotlintest-core:3.4.0")
-    testCompile("io.kotlintest:kotlintest-assertions:3.4.0")
-    testCompile("io.kotlintest:kotlintest-runner-junit5:3.4.0")
+    testImplementation("io.kotlintest:kotlintest-core:3.4.2")
+    testImplementation("io.kotlintest:kotlintest-assertions:3.4.2")
+    testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
 
     // JavaFX tests using TestFX
-    testCompile("org.testfx:testfx-core:4.0.16-alpha")
-    testCompile("org.testfx:testfx-junit:4.0.16-alpha")
+    testImplementation("org.testfx:testfx-core:4.0.16-alpha")
+    testImplementation("org.testfx:testfx-junit:4.0.16-alpha")
     // Only needed for headless testing.
 //    testCompile("org.testfx:openjfx-monocle:8u76-b04") // jdk-9+181 for Java 9
 
     // Spek
-    testCompile("org.jetbrains.spek:spek-api:1.2.1")
-    testRuntime("org.jetbrains.spek:spek-junit-platform-engine:1.2.1")
+    testImplementation("org.jetbrains.spek:spek-api:1.2.1")
+    testImplementation("org.jetbrains.spek:spek-junit-platform-engine:1.2.1")
 }
 
 repositories {
